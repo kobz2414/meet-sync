f8831afdd25bbed18bd5f9ef28f6daec83500c74
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-02-03 17:18:22 +0800

    feat: added test event data

---
+++ src/utils/constants.ts
@@ -0,0 +1,24 @@
+import { EventDetails } from "./types";
+
+export const testEvents: EventDetails[] = [
+  {
+    name: "Event 1",
+    description: "Description 1",
+    date: "2023-01-01",
+    timeStart: "10:00",
+    timeEnd: "12:00",
+    location: "Location 1",
+    participants: 10,
+    tags: ["Bembangan", "Party"],
+  },
+  {
+    name: "Event 2",
+    description: "Description 2",
+    date: "2023-01-01",
+    timeStart: "10:00",
+    timeEnd: "12:00",
+    location: "Location 2",
+    participants: 10,
+    tags: ["Test", "Sample"],
+  },
+];
