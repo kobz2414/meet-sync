aa23e01a15c450840086e34f4ade747c5f9b52de
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-02-04 18:05:31 +0800

    feat: added event details routing

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,9 +1,10 @@
 import { Calendar, Clock, MapPin, User } from "lucide-react";
 import EventTag from "./event-tag";
 import { EventDetails } from "../utils/types";
-
+import { useNavigate } from "@tanstack/react-router";
 
 const Event = ({
+  id,
   name,
   description,
   location,
@@ -13,6 +14,12 @@ const Event = ({
   participants,
   tags,
 }: EventDetails) => {
+  const navigate = useNavigate();
+
+  const handlePageDetails = (id: string) => {
+    navigate({ to: "/event-details/$id", params: { id: id } });
+  }
+
   return (
     <>
       <div className="flex md:flex-row flex-col p-4 my-2 border justify-between rounded-md border-gray-300">
@@ -42,17 +49,17 @@ const Event = ({
           </div>
           <p className="py-1">{description}</p>
           <div className="flex flex-row py-2 space-x-1">
-            {tags.map((tag) => (
-              <EventTag tag={tag} />
+            {tags.map((tag, i) => (
+              <EventTag key={i} tag={tag} />
             ))}
           </div>
         </div>
-        <div className="flex md:flex-col flex-row md:justify-center md:items-end items-start py-4">
-          <button className="px-6 py-2 my-1 border border-gray-300 rounded-md bg-gray-950 text-white min-w-30">
-            Details
-          </button>
-          <button className="px-6 py-2 my-1 not-odd:border border-gray-300 rounded-md bg-gray-950 text-white min-w-30">
+        <div className="flex md:flex-col flex-row md:justify-start md:items-end items-start md:py-0 pt-4 md:space-y-2 md:space-x-0 space-x-2">
+          <button className="px-6 py-2 border-gray-300 rounded-md bg-gray-950 text-white min-w-30">
             Join
+          </button>
+          <button className="px-6 py-2 border-gray-300 rounded-md bg-gray-950 text-white min-w-30" onClick={() => handlePageDetails(id)}>
+            Details
           </button>
         </div>
       </div>
