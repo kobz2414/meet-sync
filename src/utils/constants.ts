3dc1727afd65d22bdfeb7346cca302bbb328e540
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-02-04 13:48:45 +0800

    feat: updated test data config

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,9 @@ import { EventDetails } from "./types";
 
 export const testEvents: EventDetails[] = [
   {
-    name: "Event 1",
-    description: "Description 1",
+    name: "Lorem Ipsum Event",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     date: "2023-01-01",
     timeStart: "10:00",
     timeEnd: "12:00",
