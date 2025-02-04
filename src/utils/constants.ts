438d83bbf9bf34da390b27aecac54de85091333f
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-02-04 18:05:44 +0800

    feat: added id

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,7 @@ import { EventDetails } from "./types";
 
 export const testEvents: EventDetails[] = [
   {
+    id: "1",
     name: "Lorem Ipsum Event",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -13,6 +14,7 @@ export const testEvents: EventDetails[] = [
     tags: ["Bembangan", "Party"],
   },
   {
+    id: "2",
     name: "Event 2",
     description: "Description 2",
     date: "2023-01-01",
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,5 @@
 export type EventDetails = {
+  id: string;
   name: string;
   description: string;
   location: string;
