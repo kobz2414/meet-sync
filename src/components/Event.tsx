66b5fdb9becf30b6cf836e39255100b3fcb9cada
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-02-04 13:49:06 +0800

    fix: updated styling for responsiveness

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -15,11 +15,11 @@ const Event = ({
 }: EventDetails) => {
   return (
     <>
-      <div className="flex flex-row p-4 my-2 border rounded-md border-gray-300">
-        <div className="flex flex-col max-w-lg">
+      <div className="flex md:flex-row flex-col p-4 my-2 border justify-between rounded-md border-gray-300">
+        <div className="flex flex-col max-w-md">
           <div className="py-1">
             <p className="text-xl font-semibold">{name}</p>
-            <div className="flex flex-row space-x-4">
+            <div className="flex md:flex-row flex-col space-x-4">
               <div className="flex flex-row items-center py-1">
                 <MapPin size={16} />
                 <p className="ps-1 text-sm">{location}</p>
@@ -47,11 +47,11 @@ const Event = ({
             ))}
           </div>
         </div>
-        <div className="flex flex-col flex-1 justify-center items-end pe-2">
-          <button className="px-6 py-2 border border-gray-300 rounded-md bg-gray-950 text-white ">
+        <div className="flex md:flex-col flex-row md:justify-center md:items-end items-start py-4">
+          <button className="px-6 py-2 my-1 border border-gray-300 rounded-md bg-gray-950 text-white min-w-30">
             Details
           </button>
-          <button className="px-6 py-2 border border-gray-300 rounded-md bg-gray-950 text-white">
+          <button className="px-6 py-2 my-1 not-odd:border border-gray-300 rounded-md bg-gray-950 text-white min-w-30">
             Join
           </button>
         </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,12 +3,12 @@ const Search = () => {
     <div className="flex flex-row my-2">
       <input
         type="text"
-        className="p-2 border border-gray-300 rounded-md flex-1"
+        className="py-2 px-4 border border-gray-300 rounded-md flex-1"
         placeholder="Search..."
       />
       <div className="mx-1"></div>
       <button
-        className="p-2 border border-gray-300 rounded-md bg-gray-950 text-white"
+        className="p-2 border border-gray-300 rounded-md bg-gray-950 text-white min-w-30"
         onClick={() => console.log("Search")}
       >
         Search
