ccefc194e54394a2ee08c94150cbbf0a3d64e688
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-04-07 19:59:50 +0800

    feat: added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
