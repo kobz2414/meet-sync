49ca9b9b27d9fa642389858f7046d81ad2d7694d
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-04-07 20:23:56 +0800

    feat: update Search component to use Input and Button from ShadCN

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,11 @@
-import Button from "./Button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button"
 
 const Search = () => {
   return (
-    <div className="flex flex-row my-2">
-      <input
-        type="text"
-        className="py-2 px-4 border border-gray-300 rounded-lg flex-1"
-        placeholder="Search..."
-      />
-      <div className="mx-1"></div>
-      <Button text="Search" onClick={() => console.log("Search")}/>
+    <div className="flex w-full items-center space-x-2">
+      <Input type="text" placeholder="Seach" />
+      <Button onClick={() => console.log("Search")}>Search</Button>
     </div>
   );
 };
